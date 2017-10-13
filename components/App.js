251f325065277b5961956f606988ce9f31af8370
gundupatil <gundupatil@hotmail.com>
2017-10-14 00:19:00 +0530

    Webpack is configured to use react

--- components/App.js
+++ components/App.js
@@ -1,1 +1,12 @@
+import React, { Componet } from 'react'
 
+class App extends Componet{
+
+    render(){
+        return <div> This is definitely a React app now!</div>
+
+    }
+
+}
+
+export default App
